7b1cb53850f56de764e0443111595c04ef331ae9
itsanti <kurov.aleksandr+github@gmail.com>
2020-07-08 14:19:07 +0300

    algo01 task13

---
+++ algo01/algo_01_13.py
@@ -0,0 +1,23 @@
+# https://skillsmart.ru/algo/lvl1/a9d8.html
+
+# L = road length
+# N = stoplight count
+# track = [[1,2,3], [1,2,3]] road description
+#   [free_road, red_light, green_light]
+
+def Unmanned(L, N, track):
+    cur_t = 0
+    wait_t = 0
+    
+    for part in track:
+        cur_t += part[0] - cur_t
+        
+        awt = part[1] - (cur_t + wait_t) % (part[1] + part[2])
+        
+        if awt > 0:
+            wait_t += awt
+    
+    if cur_t < L:
+        cur_t += L - cur_t
+    
+    return cur_t + wait_t
