6784ea7fa142d3b4384f4a8b415852046b0aad11
itsanti <kurov.aleksandr+github@gmail.com>
2021-11-06 17:01:11 +0300

    aisd01 task08 HashTable

---
+++ aisd01/aisd01_08.py
@@ -0,0 +1,45 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        # в качестве value поступают строки!
+        p = 31
+        hash = 0
+        p_pow = 1
+        for char in value:
+            hash += (ord(char) - ord('a') + 1) * p_pow
+            p_pow *= p
+        return hash % self.size
+
+    def seek_slot(self, value):
+        # находит индекс пустого слота для значения, или None
+        slot = self.hash_fun(value)
+        if self.slots[slot] is not None and value != self.slots[slot]:
+            start_seek = slot
+            is_end = False
+            while self.slots[slot] is not None:
+                slot = (slot + self.step) % self.size
+                if slot <= start_seek:
+                    is_end = True
+                if is_end and slot > start_seek:
+                    return None
+        return slot
+
+    def put(self, value):
+        # записываем значение по хэш-функции
+        # возвращается индекс слота или None,
+        # если из-за коллизий элемент не удаётся
+        # разместить
+        slot = self.seek_slot(value)
+        if slot is not None:
+            self.slots[slot] = value
+        return slot
+
+    def find(self, value):
+        try:
+            return self.slots.index(value)
+        except ValueError:
+            return None
---
+++ aisd01/test_aisd01_08.py
@@ -0,0 +1,34 @@
+import pytest
+from aisd01_08 import HashTable
+
+
+@pytest.fixture
+def ht():
+    return HashTable(17, 3)
+
+
+def test_hash_fun(ht):
+    assert ht.hash_fun('aaa') == ht.hash_fun('aaa')
+    assert ht.hash_fun('aaa') != ht.hash_fun('aa')
+    assert ht.hash_fun('aab') == ht.hash_fun('aab')
+    print(ht.hash_fun('aaa'))
+    print(ht.hash_fun('aa'))
+    print(ht.hash_fun('aab'))
+
+
+def test_seek_slot(ht):
+    assert ht.seek_slot('aaa') == 7
+    assert ht.seek_slot('aa') == 15
+    assert ht.seek_slot('aab') == 16
+
+
+def test_put(ht):
+    assert ht.put('aaa') == 7
+    assert ht.put('aa') == 15
+    assert ht.put('aab') == 16
+
+
+def test_find(ht):
+    ht.put('aaa b')
+    assert ht.find('aaa b') == 10
+    assert ht.find('a') is None
