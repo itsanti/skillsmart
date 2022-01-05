2226e25adffe990febca974d5c80904518f3e144
itsanti <kurov.aleksandr+github@gmail.com>
2022-01-05 18:48:28 +0300

    feat: implement DepthFirstSearch

---
+++ aisd02/test_aisd02_10.py
@@ -0,0 +1,37 @@
+import pytest
+from aisd02_10_SimpleGraph_DepthFirstSearch import *
+
+
+@pytest.fixture
+def sg5():
+    sg5 = SimpleGraph(5)
+    sg5.AddVertex('A')
+    sg5.AddVertex('B')
+    sg5.AddVertex('C')
+    sg5.AddVertex('D')
+    sg5.AddVertex('E')
+
+    sg5.AddEdge(0, 1)
+    sg5.AddEdge(0, 2)
+    sg5.AddEdge(0, 3)
+
+    sg5.AddEdge(1, 0)
+    sg5.AddEdge(1, 3)
+    sg5.AddEdge(1, 4)
+
+    sg5.AddEdge(2, 0)
+    sg5.AddEdge(2, 3)
+
+    sg5.AddEdge(3, 0)
+    sg5.AddEdge(3, 1)
+    sg5.AddEdge(3, 2)
+    sg5.AddEdge(3, 3)
+    sg5.AddEdge(3, 4)
+
+    sg5.AddEdge(4, 1)
+    sg5.AddEdge(4, 3)
+    return sg5
+
+
+def test_DepthFirstSearch(sg5):
+    print(sg5.DepthFirstSearch(4, 2))
